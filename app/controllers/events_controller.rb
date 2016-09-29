4b43bba087792c129b13e0cd8b55f7c6c6a9ef48
Kit Pearson <dev_kit@icloud.com>
2016-09-29 01:00:49 -0700

    Remove decoraters and their uses in events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,13 @@ class EventsController < ApplicationController
   def index
     @events = Event.order(occurred_at: :desc)
     respond_to do |format|
-      format.html { @events = @events.decorate }
+      format.html
       format.atom
     end
   end
 
   def show
-    @event = Event.friendly.find(params[:id]).decorate
+    @event = Event.friendly.find(params[:id])
     @title = @event.name
   end
 end
